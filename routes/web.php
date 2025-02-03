b6b0a71f15f466ded3a7a1e95eb3882c994b18ed
Xander Alfaro <xanderalfaro7@gmail.com>
2025-02-04 07:38:20 +0800

    Route change

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 
-Route::middleware(  ['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware(  ['auth:sanctum'])->get('/auth-check', function (Request $request) {
+    if($request->user()){
+        return response()->json(['message' => 'True'], 200);
+    }});
+    
 Route::post('create-user', [UserController::class, 'createUser']);
 Route::post('login-user', [UserController::class, 'loginUser']);
